dc9efbbe28ebbe555c807d56f85136366b429ad1
alxmarchi <zyex@mail.ru>
2021-01-23 15:32:24 +0300

    start making about and production page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import CarouselBoxAbout from '../components/CarouselBoxAbout'
 
 const About =() =>{
     return (
+        <> 
+        <h1 className="m-4">Информация о компании</h1>
+    <CarouselBoxAbout></CarouselBoxAbout>
 <Container>
-    <h1 className="m-4">Информация о компании</h1>
+    
     <p>Sydler Remedies Pvt. Ltd. — Группа компаний Sydler — это ведущий производитель и экспортёр#nbsp; высококачественных и надежных лекарственных форм, фитопрепаратов, БАДов и пищевых добавок#nbsp;, а также нутрицевтиков, которые удовлетворяют взыскательные требования рынка по всему миру.</p>
     <h3 className="m-4">Сертификаты</h3>
     <h3 className="m-4">Контакты</h3>
 </Container>
+</>
     )
 }
 
--- src/pages/Production.js
+++ src/pages/Production.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import CardDeck from 'react-bootstrap/CardDeck'
+import orange_card from "../assets/images/orange_card.png"
+import mint_card from "../assets/images/mint_card.png"
 
 const Production =() =>{
     return (
 <div>
     <h1>Продукция</h1>
+    <CardDeck>
+    <Card style={{ width: '10rem', border: 0 }}>
+  <Card.Img variant="top" src={orange_card} />
+  <Card.Body>
+    <Button variant="primary">Подробнее</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '10rem', border: 0 }}>
+  <Card.Img variant="top" src={orange_card} />
+  <Card.Body>
+    <Button variant="primary">Подробнее</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '10rem', border: 0 }}>
+  <Card.Img variant="top" src={orange_card} />
+  <Card.Body>
+    <Button variant="primary">Подробнее</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '10rem', border: 0 }}>
+  <Card.Img variant="top" src={orange_card} />
+  <Card.Body>
+    <Button variant="primary">Подробнее</Button>
+  </Card.Body>
+</Card>
+</CardDeck>
 </div>
     )
 }
